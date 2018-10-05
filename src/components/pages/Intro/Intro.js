d658817ee1b8e44e4767db3ae4610b83f5bdf77b
marjika <bluegoo5@hotmail.com>
2018-10-04 18:17:19 -0600

    new routing method

--- src/components/pages/Intro/Intro.js
+++ src/components/pages/Intro/Intro.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import SoundComponent from "../../SoundComponent";
 import './Intro.css';
-import biohazard from "../../images/biohazard.jpg"
+import biohazard from "../../images/biohazard.jpg";
+import { Redirect } from 'react-router-dom';
 
 class Intro extends Component {
 
   state = {
     sound: false,
     audio: "intro"
+    //redirect: false
   };
 
   componentDidMount = () => {
@@ -17,18 +19,23 @@ class Intro extends Component {
   }
 
   stopIntro = () => {
-    setTimeout(() => {this.setState({ sound: false }), this.skipToGame()}, 19000);
+    setTimeout(() => {this.setState({ sound: false, redirect: true }), this.toGame()}, 19000);
 }
 
-  skipToGame = () => {
-      this.toGame();
-  };
+//   skipToGame = () => {
+//       this.toGame();
+//   };
 
   toGame() {
-    window.location.assign("/characters")
-}
+    this.props.history.push("/characters")
+  }
 
   render() {
+    // const { redirect } = this.state;
+  
+    // if (redirect) {
+    //   return <Redirect to='/characters'/>;
+    // }
     return (
       <div className="intro" style={{ backgroundImage: `url(${biohazard})`}}>
       <h1>Zombies are Everywhere</h1>
@@ -36,6 +43,7 @@ class Intro extends Component {
             sound={this.state.sound}
             audio={this.state.audio}
           />  
+          
 
       </div>
     );
