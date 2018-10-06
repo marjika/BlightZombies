4537a267e582b7b5c6f1054a2995a3003257a585
Maniacal Laura <maniacallaura@gmail.com>
2018-10-06 00:19:50 -0600

    FooterStyles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,24 +10,24 @@ const Footer = (props) => (
             <Instructions />
         </li>
         <li className="nav-item">
-            <div className="moves">Moves until random infection: {props.moves}|</div>
-            <div className="losses">Outbreaks: {props.outbreaks}|</div>
+            <div className="moves">Moves until random infection: {props.moves}     |</div>
+            <div className="losses">Outbreaks: {props.outbreaks}     |</div>
         </li>
         <li className="nav-item">
-            <div className="sample">DNA Sample found: {props.sample && '✔'}|</div>
-            <div className="mission1">Mission 1 Complete: {props.mission1 && '✔'}|</div>
+            <div className="sample">DNA Sample found: {props.sample && '✔'}     |</div>
+            <div className="mission1">Mission 1 Complete: {props.mission1 && '✔'}     |</div>
         </li>
         <li className="nav-item">
-            <div className="sample">Rare Plant found: {props.protein && '✔'}|</div>
-            <div className="mission1">Mission 2 Complete: {props.mission2 && '✔'}|</div>
+            <div className="sample">Rare Plant found: {props.protein && '✔'}     |</div>
+            <div className="mission1">Mission 2 Complete: {props.mission2 && '✔'}     |</div>
         </li>
         <li className="nav-item">
-            <div className="sample">Dr. Jasper found: {props.scientist && '✔'}|</div>
-            <div className="mission1">Mission 3 Complete: {props.mission3 && '✔'}|</div>
+            <div className="sample">Dr. Jasper found: {props.scientist && '✔'}     |</div>
+            <div className="mission1">Mission 3 Complete: {props.mission3 && '✔'}     |</div>
         </li>
         <li className="nav-item">
-            <div className="sample">Immune patient found: {props.immuneMan && '✔'}|</div>
-            <div className="mission1">Mission 4 Complete: {props.mission4 && '✔'}|</div>
+            <div className="sample">Immune patient found: {props.immuneMan && '✔'}     |</div>
+            <div className="mission1">Mission 4 Complete: {props.mission4 && '✔'}     |</div>
         </li>
       </ul>
   </div>
